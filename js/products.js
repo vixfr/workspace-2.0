19be1171d8dd72f8a2f35f80ce18abca3c3c460b
MariangelRondan <rondanmariangel@gmail.com>
2023-08-17 11:14:23 -0300

    css a products

--- js/products.js
+++ js/products.js
@@ -1,46 +1,54 @@
-const datosAutos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const jsonURL = "js/data.json"; 
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch(datosAutos)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error en la solicitud: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            for (modeloAuto of data.products) {
-                mostrarProducto(modeloAuto.image, modeloAuto.name, modeloAuto.cost, modeloAuto.description, modeloAuto.soldCount, "USD");
-            }
-        });
-});
-function mostrarProducto(urlImagen, nombre, precio, descripcion, cantVendidos, simboloMoneda) {
-    const container = document.getElementById("pb-5-container");
+const cartContainer = document.getElementById("autos");
+ 
+fetch(jsonURL)
+  .then(response => response.json())
+  .then(data => {
+    const products = data.products;
 
-    const divProducto = document.createElement('div');
-    divProducto.classList.add("contProducto");
+    
+    products.forEach(product => {
+      const productDiv = document.createElement('div');
+      productDiv.classList.add('product');
 
-    const imagen = document.createElement('img');
-    imagen.src = urlImagen;
-    imagen.classList.add("imgProducto");
-    divProducto.appendChild(imagen);
+      const image = document.createElement('img');
+      image.src = product.image;
+      image.classList.add("imagen")
+      productDiv.appendChild(image);
 
-    const nombreP = document.createElement('h2');
-    nombreP.textContent = nombre;
-    divProducto.appendChild(nombreP);
+const info = document.createElement('div');
+info.classList.add('info')
+productDiv.appendChild(info);
 
-    const descripcionP = document.createElement('p');
-    descripcionP.textContent = descripcion;
-    divProducto.appendChild(descripcionP);
+      const name = document.createElement('h2');
+      name.textContent = product.name;
+      // productDiv.appendChild(name);
+    info.appendChild(name);
+    name.classList.add('name')
 
-    const precioP = document.createElement('p');
-    precioP.textContent = `${simboloMoneda} ${precio}`;
-    precioP.classList.add("precioProducto");
-    divProducto.appendChild(precioP);
+      const price = document.createElement('p');
+      price.textContent = `${product.currency} ${product.cost} `;
+      // productDiv.appendChild(price);
+      info.appendChild(price);
+      price.classList.add('price')
 
-    const cantVendidosP = document.createElement('p');
-    cantVendidosP.textContent = `Vendidos: ${cantVendidos}`;
-    divProducto.appendChild(cantVendidosP);
 
-    container.appendChild(divProducto);
-};
+      const soldCount = document.createElement('p');
+      soldCount.textContent = `Vendidos: ${product.soldCount}`;
+      productDiv.appendChild(soldCount);
+     soldCount.classList.add('vendidos')
+
+
+      const description = document.createElement('p');
+      description.textContent = product.description;
+      // productDiv.appendChild(description);
+      info.appendChild(description);
+
+      cartContainer.appendChild(productDiv);
+     
+    });
+  })
+  .catch(error => {
+    console.error('Error al cargar los datos:', error);
+  });
