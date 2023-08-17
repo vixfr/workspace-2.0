f024fd9505b777e44d9abfd8476091066738e6db
alxrolande <141145424+AlxRolande@users.noreply.github.com>
2023-08-17 12:44:15 -0300

    Revert "Merge branch 'main' of https://github.com/vixfr/workspace-2.0"
    This reverts commit 272105ceb8084e09f2f06df7653b05d493982235, reversing
    changes made to 226e2dc32da9d343317f6719548b912b3d1fda31.

--- js/products.js
+++ js/products.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function mostrarProducto(urlImagen, nombre, precio, descripcion, cantVendidos, simboloMoneda) {
     const container = document.getElementById("pb-5-container");
 
-<<<<<<< Updated upstream
     const divProducto = document.createElement('div');
     divProducto.classList.add("contProducto");
 
@@ -35,18 +34,12 @@ function mostrarProducto(urlImagen, nombre, precio, descripcion, cantVendidos, s
     precioP.textContent = `${simboloMoneda} ${precio}`;
     precioP.classList.add("precioProducto");
     divProducto.appendChild(precioP);
-=======
-    products.forEach(product => {
-      const productDiv = document.createElement('div');
-      productDiv.classList.add('product');
->>>>>>> Stashed changes
 
     const descripcionP = document.createElement('p');
     descripcionP.textContent = descripcion;
     descripcionP.id="descripcion"
     divProducto.appendChild(descripcionP);
 
-<<<<<<< Updated upstream
 
     const cantVendidosP = document.createElement('p');
     cantVendidosP.textContent = `Vendidos: ${cantVendidos}`;
@@ -54,23 +47,3 @@ function mostrarProducto(urlImagen, nombre, precio, descripcion, cantVendidos, s
 
     container.appendChild(divProducto);
 };
-=======
-      const soldCount = document.createElement('p');
-      soldCount.textContent = `Vendidos: ${product.soldCount}`;
-      productDiv.appendChild(soldCount);
-     soldCount.classList.add('vendidos')
-
-
-      const description = document.createElement('p');
-      description.textContent = product.description;
-      // productDiv.appendChild(description);
-      info.appendChild(description);
-
-      cartContainer.appendChild(productDiv);
-     
-    });
-  })
-  .catch(error => {
-    console.error('Error al cargar los datos:', error);
-  });
->>>>>>> Stashed changes
