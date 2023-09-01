fa24bd02aa04097511f67d802ae7f3496d8ac993
MariangelRondan <rondanmariangel@gmail.com>
2023-08-31 22:51:33 -0300

    searchBar

--- js/buscador.js
+++ js/buscador.js
@@ -1,9 +1,109 @@
+const urls = [
+  "https://japceibal.github.io/emercado-api/cats_products/101.json",
+  "https://japceibal.github.io/emercado-api/cats_products/102.json",
+  "https://japceibal.github.io/emercado-api/cats_products/103.json",
+  "https://japceibal.github.io/emercado-api/cats_products/104.json",
+  "https://japceibal.github.io/emercado-api/cats_products/105.json",
+  "https://japceibal.github.io/emercado-api/cats_products/106.json",
+  "https://japceibal.github.io/emercado-api/cats_products/107.json",
+  "https://japceibal.github.io/emercado-api/cats_products/108.json",
+  "https://japceibal.github.io/emercado-api/cats_products/109.json",
+];
+
+const fetchPromises = urls.map((url) => fetch(url));
+
 document.addEventListener("DOMContentLoaded", () => {
-    const buscador = document.getElementById("buscador");
-    const contenedor = document.getElementById("contenedor");
-    buscador.addEventListener("click", () => {
-        while (contenedor.firstChild) {
-            contenedor.removeChild(contenedor.firstChild);
+  const buscador = document.getElementById("buscador");
+  const searchBar = document.getElementById("searchBar");
+  const contenedor = document.getElementById("contenedor");
+
+  buscador.addEventListener("click", () => {
+    while (contenedor.firstChild) {
+      contenedor.removeChild(contenedor.firstChild);
+    }
+
+    Promise.all(fetchPromises)
+      .then((responses) => {
+        return Promise.all(
+          responses.map((response) => response.clone().json())
+        );
+      })
+      .then((dataArray) => {
+        //array que contiene los objetos extraidos de las apis [{autos},{juguetes}, {muebles}, etc]
+        const searchTerm = searchBar.value.toLowerCase(); // Convertir el valor a minúsculas
+        const matches = [];
+
+        for (const data of dataArray) {
+          //por cada objeto dentro de ese array
+          for (const product of data.products) {
+            //accede al arreglo data.products, e itera cada elemento del arreglo (cada producto)
+            const name = product.name.toLowerCase();
+            const description = product.description.toLowerCase();
+
+            // Buscar similitudes en el nombre o la descripción
+            if (name.includes(searchTerm) || description.includes(searchTerm)) {
+              matches.push(product);
+            }
+          }
         }
-    })
-})
+
+        // Aquí tienes los productos que coinciden con la búsqueda
+        for (const match of matches) {
+          // Agregar lógica para mostrar los resultados en el contenedor
+          mostrarProducto(
+            match.image,
+            match.name,
+            match.cost,
+            match.description,
+            match.soldCount,
+            match.currency
+          );
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
+
+//  LA QUISE EXPORTAR CON EXPORT {MOSTRARPRODUCTO} PERO SE ROMPIO. PODEMOS INTENTARLO...
+
+function mostrarProducto(
+  urlImagen,
+  nombre,
+  precio,
+  descripcion,
+  cantVendidos,
+  simboloMoneda
+) {
+  const container = document.getElementById("contenedor");
+
+  const divProducto = document.createElement("div");
+  divProducto.classList.add("contProducto");
+
+  const imagen = document.createElement("img");
+  imagen.src = urlImagen;
+  imagen.classList.add("imgProducto");
+  divProducto.appendChild(imagen);
+
+  const nombreP = document.createElement("h2");
+  nombreP.textContent = nombre;
+  divProducto.appendChild(nombreP);
+  nombreP.classList.add("nombre");
+
+  const precioP = document.createElement("p");
+  precioP.textContent = `${simboloMoneda} ${precio}`;
+  precioP.classList.add("precioProducto");
+  divProducto.appendChild(precioP);
+
+  const descripcionP = document.createElement("p");
+  descripcionP.textContent = descripcion;
+  descripcionP.id = "descripcion";
+  divProducto.appendChild(descripcionP);
+
+  const cantVendidosP = document.createElement("p");
+  cantVendidosP.textContent = `Vendidos: ${cantVendidos}`;
+  divProducto.appendChild(cantVendidosP);
+
+  container.appendChild(divProducto);
+}
--- js/products.js
+++ js/products.js
@@ -1,200 +1,237 @@
 const iDLocalStorage = localStorage.getItem("catID");
-const datosProductos = "https://japceibal.github.io/emercado-api/cats_products/" + iDLocalStorage + ".json";
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetch(datosProductos)
-
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error en la solicitud: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.products.length === 0) {
-                const container = document.getElementById('contenedor');
-                const avisoH2 = document.createElement('h2');
-                avisoH2.id = "avisoNoProductos"
-                avisoH2.textContent = "No hay productos para mostrar";
-                container.appendChild(avisoH2);
-            } else {
-                for (producto of data.products) {
-
-                    mostrarProducto(producto.image, producto.name, producto.cost, producto.description, producto.soldCount, producto.currency);
-
-                }
-            }
-        });
-});
-function mostrarProducto(urlImagen, nombre, precio, descripcion, cantVendidos, simboloMoneda) {
-    const container = document.getElementById("contenedor");
-
-    const divProducto = document.createElement('div');
-    divProducto.classList.add("contProducto");
-
-    const imagen = document.createElement('img');
-    imagen.src = urlImagen;
-    imagen.classList.add("imgProducto");
-    divProducto.appendChild(imagen);
-
-    const nombreP = document.createElement('h2');
-    nombreP.textContent = nombre;
-    divProducto.appendChild(nombreP);
-    nombreP.classList.add("nombre");
-
-    const precioP = document.createElement('p');
-    precioP.textContent = `${simboloMoneda} ${precio}`;
-    precioP.classList.add("precioProducto");
-    divProducto.appendChild(precioP);
-
-    const descripcionP = document.createElement('p');
-    descripcionP.textContent = descripcion;
-    descripcionP.id = "descripcion"
-    divProducto.appendChild(descripcionP);
-
-
-    const cantVendidosP = document.createElement('p');
-    cantVendidosP.textContent = `Vendidos: ${cantVendidos}`;
-    divProducto.appendChild(cantVendidosP);
-
-    container.appendChild(divProducto);
-};
-
-
-
-
+const datosProductos =
+  "https://japceibal.github.io/emercado-api/cats_products/" +
+  iDLocalStorage +
+  ".json";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const contenedor = document.getElementById("contenedor");
-    const menorPrecio = document.getElementById("menorPrecio");
-    const mayorPrecio = document.getElementById("mayorPrecio");
-    const cantVendidos = document.getElementById("cantVendidos");
-    const rangeFilterBtn = document.getElementById("rangeFilterCount");
+  fetch(datosProductos)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.products.length === 0) {
+        const container = document.getElementById("contenedor");
+        const avisoH2 = document.createElement("h2");
+        avisoH2.id = "avisoNoProductos";
+        avisoH2.textContent = "No hay productos para mostrar";
+        container.appendChild(avisoH2);
+      } else {
+        for (producto of data.products) {
+          mostrarProducto(
+            producto.image,
+            producto.name,
+            producto.cost,
+            producto.description,
+            producto.soldCount,
+            producto.currency
+          );
+        }
+      }
+    });
+});
+function mostrarProducto(
+  urlImagen,
+  nombre,
+  precio,
+  descripcion,
+  cantVendidos,
+  simboloMoneda
+) {
+  const container = document.getElementById("contenedor");
+
+  const divProducto = document.createElement("div");
+  divProducto.classList.add("contProducto");
+
+  const imagen = document.createElement("img");
+  imagen.src = urlImagen;
+  imagen.classList.add("imgProducto");
+  divProducto.appendChild(imagen);
+
+  const nombreP = document.createElement("h2");
+  nombreP.textContent = nombre;
+  divProducto.appendChild(nombreP);
+  nombreP.classList.add("nombre");
+
+  const precioP = document.createElement("p");
+  precioP.textContent = `${simboloMoneda} ${precio}`;
+  precioP.classList.add("precioProducto");
+  divProducto.appendChild(precioP);
+
+  const descripcionP = document.createElement("p");
+  descripcionP.textContent = descripcion;
+  descripcionP.id = "descripcion";
+  divProducto.appendChild(descripcionP);
+
+  const cantVendidosP = document.createElement("p");
+  cantVendidosP.textContent = `Vendidos: ${cantVendidos}`;
+  divProducto.appendChild(cantVendidosP);
+
+  container.appendChild(divProducto);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.getElementById("contenedor");
+  const menorPrecio = document.getElementById("menorPrecio");
+  const mayorPrecio = document.getElementById("mayorPrecio");
+  const cantVendidos = document.getElementById("cantVendidos");
+  const rangeFilterBtn = document.getElementById("rangeFilterCount");
   const clearRangeFilterBtn = document.getElementById("clearRangeFilter");
 
+  mayorPrecio.addEventListener("click", () => {
+    while (contenedor.firstChild) {
+      contenedor.removeChild(contenedor.firstChild);
+    }
 
-    mayorPrecio.addEventListener("click", () => {
-        while (contenedor.firstChild) {
-            contenedor.removeChild(contenedor.firstChild);
+    fetch(datosProductos)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error en la solicitud: ${response.status}`);
         }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.products.length === 0) {
+          const container = document.getElementById("contenedor");
+          const avisoH2 = document.createElement("h2");
+          avisoH2.id = "avisoNoProductos";
+          avisoH2.textContent = "No hay productos para mostrar";
+          container.appendChild(avisoH2);
+        } else {
+          for (producto of data.products.sort((a, b) => b.cost - a.cost)) {
+            mostrarProducto(
+              producto.image,
+              producto.name,
+              producto.cost,
+              producto.description,
+              producto.soldCount,
+              producto.currency
+            );
+          }
+        }
+      });
+  });
+  menorPrecio.addEventListener("click", () => {
+    while (contenedor.firstChild) {
+      contenedor.removeChild(contenedor.firstChild);
+    }
 
-        fetch(datosProductos)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error en la solicitud: ${response.status}`);
-                }
-                return response.json();
+    fetch(datosProductos)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.products.length === 0) {
+          const container = document.getElementById("contenedor");
+          const avisoH2 = document.createElement("h2");
+          avisoH2.id = "avisoNoProductos";
+          avisoH2.textContent = "No hay productos para mostrar";
+          container.appendChild(avisoH2);
+        } else {
+          for (producto of data.products.sort((a, b) => a.cost - b.cost)) {
+            mostrarProducto(
+              producto.image,
+              producto.name,
+              producto.cost,
+              producto.description,
+              producto.soldCount,
+              producto.currency
+            );
+          }
+        }
+      });
+  });
+  cantVendidos.addEventListener("click", () => {
+    while (contenedor.firstChild) {
+      contenedor.removeChild(contenedor.firstChild);
+    }
+
+    fetch(datosProductos)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.products.length === 0) {
+          const container = document.getElementById("contenedor");
+          const avisoH2 = document.createElement("h2");
+          avisoH2.id = "avisoNoProductos";
+          avisoH2.textContent = "No hay productos para mostrar";
+          container.appendChild(avisoH2);
+        } else {
+          for (producto of data.products.sort(
+            (a, b) => b.soldCount - a.soldCount
+          )) {
+            mostrarProducto(
+              producto.image,
+              producto.name,
+              producto.cost,
+              producto.description,
+              producto.soldCount,
+              producto.currency
+            );
+          }
+        }
+      });
+  });
+
+  rangeFilterBtn.addEventListener("click", () => {
+    while (contenedor.firstChild) {
+      contenedor.removeChild(contenedor.firstChild);
+    }
+
+    const minPrice = parseFloat(
+      document.getElementById("rangeFilterCountMin").value
+    );
+    const maxPrice = parseFloat(
+      document.getElementById("rangeFilterCountMax").value
+    );
+
+    fetch(datosProductos)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.products.length === 0) {
+          const container = document.getElementById("contenedor");
+          const avisoH2 = document.createElement("h2");
+          avisoH2.id = "avisoNoProductos";
+          avisoH2.textContent = "No hay productos para mostrar";
+          container.appendChild(avisoH2);
+        } else {
+          const filteredAndSortedProducts = data.products
+            .filter((product) => {
+              const productPrice = parseFloat(product.cost);
+              return productPrice >= minPrice && productPrice <= maxPrice;
             })
-            .then(data => {
-                if (data.products.length === 0) {
-                    const container = document.getElementById('contenedor');
-                    const avisoH2 = document.createElement('h2');
-                    avisoH2.id = "avisoNoProductos"
-                    avisoH2.textContent = "No hay productos para mostrar";
-                    container.appendChild(avisoH2);
-                } else {
-                    for (producto of data.products.sort((a, b) => b.cost - a.cost)) {
-
-                        mostrarProducto(producto.image, producto.name, producto.cost, producto.description, producto.soldCount, producto.currency);
-
-                    }
-                }
-            });
-    })
-    menorPrecio.addEventListener("click", () => {
-        while (contenedor.firstChild) {
-            contenedor.removeChild(contenedor.firstChild);
+            .sort((a, b) => a.cost - b.cost);
+          for (const producto of filteredAndSortedProducts) {
+            mostrarProducto(
+              producto.image,
+              producto.name,
+              producto.cost,
+              producto.description,
+              producto.soldCount,
+              producto.currency
+            );
+          }
         }
+      });
+  });
 
-
-        fetch(datosProductos)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error en la solicitud: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.products.length === 0) {
-                    const container = document.getElementById('contenedor');
-                    const avisoH2 = document.createElement('h2');
-                    avisoH2.id = "avisoNoProductos"
-                    avisoH2.textContent = "No hay productos para mostrar";
-                    container.appendChild(avisoH2);
-                } else {
-                    for (producto of data.products.sort((a, b) => a.cost - b.cost)) {
-                        mostrarProducto(producto.image, producto.name, producto.cost, producto.description, producto.soldCount, producto.currency);
-                    }
-                }
-            });
-    })
-    cantVendidos.addEventListener("click", () => {
-        while (contenedor.firstChild) {
-            contenedor.removeChild(contenedor.firstChild);
-        }
-
-
-        fetch(datosProductos)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error en la solicitud: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.products.length === 0) {
-                    const container = document.getElementById('contenedor');
-                    const avisoH2 = document.createElement('h2');
-                    avisoH2.id = "avisoNoProductos"
-                    avisoH2.textContent = "No hay productos para mostrar";
-                    container.appendChild(avisoH2);
-                } else {
-                    for (producto of data.products.sort((a, b) => b.soldCount - a.soldCount)) {
-                        mostrarProducto(producto.image, producto.name, producto.cost, producto.description, producto.soldCount, producto.currency);
-                    }
-                }
-            });
-    })
-
-    rangeFilterBtn.addEventListener("click", () => {
-        while (contenedor.firstChild) {
-          contenedor.removeChild(contenedor.firstChild);
-        }
-    
-        const minPrice = parseFloat(document.getElementById("rangeFilterCountMin").value);
-        const maxPrice = parseFloat(document.getElementById("rangeFilterCountMax").value);
-    
-        fetch(datosProductos)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error(`Error en la solicitud: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then(data => {
-            if (data.products.length === 0) {
-                const container = document.getElementById('contenedor');
-                const avisoH2 = document.createElement('h2');
-                avisoH2.id = "avisoNoProductos"
-                avisoH2.textContent = "No hay productos para mostrar";
-                container.appendChild(avisoH2);
-            } else {
-                const filteredAndSortedProducts = data.products.filter(product => {
-                    const productPrice = parseFloat(product.cost);
-                    return productPrice >= minPrice && productPrice <= maxPrice;
-                }).sort((a, b) => a.cost - b.cost);
-                for (const producto of filteredAndSortedProducts) {
-                    mostrarProducto(producto.image, producto.name, producto.cost, producto.description, producto.soldCount, producto.currency);
-                  }
-                }
-              });
-          });
-        
-          clearRangeFilterBtn.addEventListener("click", () => {
-            document.getElementById("rangeFilterCountMin").value = "";
-            document.getElementById("rangeFilterCountMax").value = "";
-
-           
-                
-});
+  clearRangeFilterBtn.addEventListener("click", () => {
+    document.getElementById("rangeFilterCountMin").value = "";
+    document.getElementById("rangeFilterCountMax").value = "";
+  });
 });
