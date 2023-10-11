a4749ce6a9371e4baa2a308805965a1c4a458285
alxrolande <141145424+AlxRolande@users.noreply.github.com>
2023-10-10 23:57:19 -0300

    Modifico envio , se muestra solo si hay elementos

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tbody = document.getElementById("tbody");
+  const carritoDiv = document.getElementById("carrito-div");
 
   // Obtén el carrito del localStorage
   let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -8,47 +9,55 @@ document.addEventListener("DOMContentLoaded", () => {
   const mostrarCarrito = () => {
     tbody.innerHTML = ""; // Limpia el contenedor actual
 
-    // Itera a través del arreglo de IDs de productos en el carrito
-    carrito.forEach((producto, index) => {
-      // Crea elementos HTML para mostrar los detalles del producto y agrégalos a tbody
-      const filaProducto = document.createElement("tr");
+    if (carrito.length > 0) {
+      // El carrito tiene elementos, muestra el carrito
+      carritoDiv.style.display = "block";
 
-      const imagenProducto = document.createElement("td");
-      const imagen = document.createElement("img");
-      imagen.src = producto.images[0];
-      imagenProducto.appendChild(imagen);
-      filaProducto.appendChild(imagenProducto);
+      // Itera a través del arreglo de IDs de productos en el carrito
+      carrito.forEach((producto, index) => {
+        // Crea elementos HTML para mostrar los detalles del producto y agrégalos a tbody
+        const filaProducto = document.createElement("tr");
 
-      const nombreProducto = document.createElement("td");
-      nombreProducto.textContent = producto.name;
-      filaProducto.appendChild(nombreProducto);
+        const imagenProducto = document.createElement("td");
+        const imagen = document.createElement("img");
+        imagen.src = producto.images[0];
+        imagenProducto.appendChild(imagen);
+        filaProducto.appendChild(imagenProducto);
 
-      const costoProducto = document.createElement("td");
-      costoProducto.textContent = producto.cost;
-      filaProducto.appendChild(costoProducto);
+        const nombreProducto = document.createElement("td");
+        nombreProducto.textContent = producto.name;
+        filaProducto.appendChild(nombreProducto);
 
-      const cantidadProducto = document.createElement("td");
-      const cantidadInput = document.createElement("input");
-      cantidadInput.className = "form-control form-control-color";
-      cantidadInput.type = "number";
-      cantidadInput.id = `cantidad${index}`; // Agrega un ID único para el input
-      cantidadInput.addEventListener("input", actualizarSubtotal); // Agrega un evento input
-      cantidadProducto.appendChild(cantidadInput);
-      filaProducto.appendChild(cantidadProducto);
+        const costoProducto = document.createElement("td");
+        costoProducto.textContent = producto.cost;
+        filaProducto.appendChild(costoProducto);
 
-      const subTotal = document.createElement("td");
-      subTotal.innerHTML = `<p id="subtotal${index}">subtotal</p>`;
-      filaProducto.appendChild(subTotal);
+        const cantidadProducto = document.createElement("td");
+        const cantidadInput = document.createElement("input");
+        cantidadInput.className = "form-control form-control-color";
+        cantidadInput.type = "number";
+        cantidadInput.id = `cantidad${index}`; // Agrega un ID único para el input
+        cantidadInput.addEventListener("input", actualizarSubtotal); // Agrega un evento input
+        cantidadProducto.appendChild(cantidadInput);
+        filaProducto.appendChild(cantidadProducto);
 
-      const eliminarProducto = document.createElement("td"); // Agregar columna para el botón Eliminar
-      const botonEliminar = document.createElement("button");
-      botonEliminar.textContent = "Eliminar";
-      botonEliminar.addEventListener("click", () => eliminarProductoDelCarrito(index)); // Agregar evento para eliminar
-      eliminarProducto.appendChild(botonEliminar);
-      filaProducto.appendChild(eliminarProducto);
+        const subTotal = document.createElement("td");
+        subTotal.innerHTML = `<p id="subtotal${index}">subtotal</p>`;
+        filaProducto.appendChild(subTotal);
 
-      tbody.appendChild(filaProducto);
-    });
+        const eliminarProducto = document.createElement("td"); // Agregar columna para el botón Eliminar
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.addEventListener("click", () => eliminarProductoDelCarrito(index)); // Agregar evento para eliminar
+        eliminarProducto.appendChild(botonEliminar);
+        filaProducto.appendChild(eliminarProducto);
+
+        tbody.appendChild(filaProducto);
+      });
+    } else {
+      // El carrito está vacío, oculta el carrito
+      carritoDiv.style.display = "none";
+    }
   };
 
   // Función para actualizar el subtotal cuando se cambia la cantidad
