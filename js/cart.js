434d0ce2e3c55f46a3d45107a27bc8e507614d2c
MariangelRondan <rondanmariangel@gmail.com>
2023-10-20 08:14:08 -0300

    validaciones

--- js/cart.js
+++ js/cart.js
@@ -179,7 +179,28 @@ function actualizarSumaTotal() {
   let totalNum = sumaSubtotal + precioEnvioNum;
 
   total.textContent = "UYU " + totalNum.toFixed(2);
-
-  // const totalNum = parseFloat(TOTAL).toFixed(2);
-  // total.textContent = "UYU " + totalNum;
 }
+
+// VALIDACION BOOSTRAP
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
